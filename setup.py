afceb5edd1e8efe64d8fde48b86d0037182d9f1b
Nick Gryga <nickolasgryga@gmail.com>
2019-01-22 14:36:52 -0600

    setting up dir structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+'''oracle_eloqua'''
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='oracle_eloqua',
+    version='0.0.1',
+    description='Python hooks for Oracle Eloqua API',
+    long_description=readme,
+    author='Nick Gryga',
+    author_email='nickolasgryga@gmail.com'
+)
+
+
