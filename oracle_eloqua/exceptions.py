cfb841f4d3d797d38cca1ea93b0062add12a0852
Nick Gryga <nickolasgryga@gmail.com>
2019-01-23 15:37:18 -0600

    added some exceptions

--- oracle_eloqua/exceptions.py
+++ oracle_eloqua/exceptions.py
@@ -1,5 +1,17 @@
 class AuthorizationError(Exception):
-    '''Exception raised when user does not have appropriate credentials'''
+    ''' Exception raised when user does not have appropriate credentials
+        Used for the following HTTP error codes:
+            301, 401            
+    '''
     def __init__(self, expression, message):
         self.message = message
+
+class ForbiddenError(Exception):
+    ''' Exception raised when user does not have appropriate credentials
+        Used for the following HTTP error codes:
+            403
+    '''
+    def __init__(self, expression, message):
+        self.message = message
+
 
