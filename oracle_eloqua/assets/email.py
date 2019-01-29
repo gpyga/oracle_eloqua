05cd449a7789f0e475ae59709ac402af717541e2
Nick Gryga <nickolasgryga@gmail.com>
2019-01-29 10:30:56 -0600

    defining objects

--- oracle_eloqua/assets/email.py
+++ oracle_eloqua/assets/email.py
@@ -1,8 +1,18 @@
-from oracle_eloqua.api import EloquaRequest
+from oracle_eloqua.api import EloquaRequest, EloquaApi
 
 
 class Email:
-    def __init__(self):
+    def __init__(self, asset_id, api=None):
+        self._asset_id = asset_id # maybe int or array?
+        self._api = api or EloquaApi.get_default_api()
+        
+    @classmethod
+    def from_search(cls, search, api=None):
+        # method for pulling asset_id from a search query
         pass
 
+    @classmethod
+    def create(cls, params)
+
+    def get(self):
 
---
+++ oracle_eloqua/objects.py
@@ -0,0 +1,16 @@
+from oracle_eloqua.api import EloquaRequest, EloquaApi
+
+class EloquaObject:
+    """ Generic object class for Eloqua objects.
+    """
+    def __init__(self, id):
+        pass
+
+
+class EloquaQuery:
+    """ Generic object class for Eloqua queries (ie - campaigns)
+        
+    """
+
+    def __init__(self, **kwargs):
+        pass
