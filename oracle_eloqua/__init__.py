ebc46c83050c117be0f3fda523ff14d52bdc628e
Nick Gryga <nickolasgryga@gmail.com>
2019-02-01 15:55:04 -0600

    additions to __init__

--- oracle_eloqua/__init__.py
+++ oracle_eloqua/__init__.py
@@ -1,4 +1,5 @@
 import os
 
-from oracle_eloqua.api import EloquaApi, EloquaRequest, Cursor
 from oracle_eloqua.config import API_VERSION, LOGIN_URL
+from oracle_eloqua.api import EloquaApi, EloquaRequest, Cursor
+from oracle_eloqua.objects import EloquaObject
